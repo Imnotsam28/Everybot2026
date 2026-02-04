bec79d8fad8fcd40a2599c46501f6847db72778a
JSP <jobanpreet.s.pannu@gmail.com>
2026-02-03 18:52:51 -0600

    Shooter part 2

--- src/main/java/team5427/frc/robot/subsystems/Shooter/io/ShooterIO.java
+++ src/main/java/team5427/frc/robot/subsystems/Shooter/io/ShooterIO.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.Celsius;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
 import org.checkerframework.checker.units.qual.degrees;
@@ -12,6 +13,7 @@ import org.littletonrobotics.junction.AutoLog;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.LinearAcceleration;
@@ -24,6 +26,7 @@ public interface ShooterIO {
     public static class ShooterIOInputs {
         public LinearVelocity ShooterMotorMasterLinearVelocity = MetersPerSecond.of(0.0);
         public AngularVelocity ShooterMotorMasterAngularVelocity = RotationsPerSecond.of(0.0);
+        public AngularAcceleration shooterMotorMasterAngularAcceleration = RotationsPerSecondPerSecond.of(0.0);
         public LinearAcceleration ShooterMotorMasterAcceleration = MetersPerSecondPerSecond.of(0.0);
         public Voltage ShooterMotorMasterVoltage = Volts.of(0.0);
         public Current ShooterMotorMasterCurrent = Amps.of(0.0);
@@ -31,12 +34,13 @@ public interface ShooterIO {
         public boolean ShooterMotorMasterConnected = false;
         public boolean ShooterMotorMasterDisabled = false;
 
-        public LinearVelocity kShooterMotorSlaveLinearVelocity = MetersPerSecond.of(0.0);
-        public AngularVelocity kShooterMotorSlaveAngularVelocity = RotationsPerSecond.of(0.0);
-        public LinearAcceleration kShooterMotorSlaveAcceleration = MetersPerSecondPerSecond.of(0.0);
-        public Voltage kShooterMotorSlaveVoltage = Volts.of(0.0);
-        public Current kShooterMotorSlaveCurrent = Amps.of(0.0);
-        public Temperature kShooterMotorSlaveTemperature = Celsius.of(0.0);
+        public LinearVelocity ShooterMotorSlaveLinearVelocity = MetersPerSecond.of(0.0);
+        public AngularVelocity ShooterMotorSlaveAngularVelocity = RotationsPerSecond.of(0.0);
+        public AngularAcceleration shooterMotorSlaveAngularAcceleration = RotationsPerSecondPerSecond.of(0.0);
+        public LinearAcceleration ShooterMotorSlaveAcceleration = MetersPerSecondPerSecond.of(0.0);
+        public Voltage ShooterMotorSlaveVoltage = Volts.of(0.0);
+        public Current ShooterMotorSlaveCurrent = Amps.of(0.0);
+        public Temperature ShooterMotorSlaveTemperature = Celsius.of(0.0);
         public boolean ShooterMotorSlaveConnected = false;
         public boolean ShooterMotorSlaveDisabled = false;
     }
